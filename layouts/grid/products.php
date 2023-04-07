8dafd248a67352efe5de788a7d73c63c7b22c1f0
devonriley <devontriley@gmail.com>
2023-04-07 12:41:53 -0400

    Allow product grid to select which h tag to use

--- layouts/grid/products.php
+++ layouts/grid/products.php
@@ -5,8 +5,8 @@ $products = get_sub_field('products');
 <div class="container-lg">
     <?php if ( $header ) : ?>
         <div class="row">
-            <div class="col-sm-10 offset-sm-1 col-md-4 col-lg-12 offset-lg-0">
-                <h2><?php echo $header ?></h2>
+            <div class="header text-center col-sm-10 offset-sm-1 col-md-4 col-lg-12 offset-lg-0">
+                <<?php echo $headerSize ?>><?php echo $header ?></<?php echo $headerSize ?>>
             </div>
         </div>
     <?php endif; ?>
