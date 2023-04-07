e6f8279797ee628f4be0185ff4c471cb1530ab4d
devonriley <devontriley@gmail.com>
2023-04-07 09:05:33 -0400

    Add <a> around artist card in grid module and update styling

--- layouts/grid/artist-profiles-card.php
+++ layouts/grid/artist-profiles-card.php
@@ -1,15 +1,18 @@
 <?php
 $id = $artist->ID;
 $featuredImage = get_the_post_thumbnail( $id );
+$permalink = get_permalink( $id );
 ?>
 
 <div class="text-center position-relative">
-    <!-- Image -->
-    <?php if ( $featuredImage ) : ?>
-        <?php echo $featuredImage ?>
-    <?php endif; ?>
-    <!-- Header -->
-    <?php if( $artist->post_title ) : ?>
-        <h3><?php echo $artist->post_title ?></h3>
-    <?php endif; ?>
+    <a href="<?php echo $permalink ?>">
+        <!-- Image -->
+        <?php if ( $featuredImage ) : ?>
+            <?php echo $featuredImage ?>
+        <?php endif; ?>
+        <!-- Header -->
+        <?php if( $artist->post_title ) : ?>
+            <h3><?php echo $artist->post_title ?></h3>
+        <?php endif; ?>
+    </a>
 </div>
