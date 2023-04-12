461dcbd2d3fef7504d99cf15857d9e7397c16f15
devonriley <devontriley@gmail.com>
2023-04-12 07:08:59 -0400

    Style posts navigation

--- single.php
+++ single.php
@@ -34,19 +34,16 @@ get_header();
 //		comments_template();
 //	}
 //
-//	// Previous/next post navigation.
-//	$twentytwentyone_next = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' );
-//	$twentytwentyone_prev = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' );
-//
-//	$twentytwentyone_next_label     = esc_html__( 'Next post', 'twentytwentyone' );
-//	$twentytwentyone_previous_label = esc_html__( 'Previous post', 'twentytwentyone' );
-//
-//	the_post_navigation(
-//		array(
-//			'next_text' => '<p class="meta-nav">' . $twentytwentyone_next_label . $twentytwentyone_next . '</p><p class="post-title">%title</p>',
-//			'prev_text' => '<p class="meta-nav">' . $twentytwentyone_prev . $twentytwentyone_previous_label . '</p><p class="post-title">%title</p>',
-//		)
-//	);
+	// Previous/next post navigation.
+	$heretic_next = is_rtl() ? heretic_get_icon_svg( 'ui', 'arrow_left' ) : heretic_get_icon_svg( 'ui', 'arrow_right' );
+	$heretic_prev = is_rtl() ? heretic_get_icon_svg( 'ui', 'arrow_right' ) : heretic_get_icon_svg( 'ui', 'arrow_left' );
+
+	the_post_navigation(
+		array(
+			'next_text' => '<p class="post-title">%title</p><p class="meta-nav">' . $heretic_next . '</p>',
+			'prev_text' => '<p class="meta-nav">' . $heretic_prev . '</p><p class="post-title">%title</p>',
+		)
+	);
     endwhile;
     ?>
 </div>
