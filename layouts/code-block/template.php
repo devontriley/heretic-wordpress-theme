79a7b960681fa8a41b157bea9c0da617d293508f
devonriley <devontriley@gmail.com>
2023-04-18 13:51:49 -0400

    Make code block content full width

--- layouts/code-block/template.php
+++ layouts/code-block/template.php
@@ -6,11 +6,7 @@ $code = get_sub_field( 'code' );
 
 <div class="layout code-block <?php if ( $is_preview ) { echo 'is-preview'; } ?>" data-layout-count="<?php echo $layoutCounter ?>">
     <div class="container-lg">
-        <div class="row">
-            <div class="col-sm-10 offset-sm-1">
-                <?php echo $code ?>
-            </div>
-        </div>
+        <?php echo $code ?>
     </div>
 </div>
 
