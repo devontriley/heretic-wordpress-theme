9afb5ab06ddec25c71467f0c092240cd80d1c0e7
devonriley <devontriley@gmail.com>
2023-04-21 10:44:00 -0400

    Add header and body copy to code block (for clubhouses fb feeds)

--- layouts/code-block/template.php
+++ layouts/code-block/template.php
@@ -1,11 +1,24 @@
 <?php
 global $layoutCounter;
-
+$header = get_sub_field('header');
+$headerSize = get_sub_field( 'header_size' );
+$bodyCopy = get_sub_field('body_copy');
 $code = get_sub_field( 'code' );
 ?>
 
 <div class="layout-code-block layout-vertical-spacing <?php if ( $is_preview ) { echo 'is-preview'; } ?>" data-layout-count="<?php echo $layoutCounter ?>">
     <div class="container-lg">
+        <div class="row">
+            <div class="header text-center col-sm-10 col-md-6 offset-sm-1 offset-md-3">
+                <<?php echo $headerSize ?>>
+                    <?php echo $header ?>
+                </<?php echo $headerSize ?>>
+                <?php
+                if ( $bodyCopy ) {
+                    echo $bodyCopy;
+                } ?>
+            </div>
+        </div>
         <?php echo $code ?>
     </div>
 </div>
