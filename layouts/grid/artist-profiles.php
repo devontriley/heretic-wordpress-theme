c806e17d070ca30fe26779c5eef6ac9f64f694d6
devonriley <devontriley@gmail.com>
2023-04-21 13:52:25 -0400

    Allow artist grid to be a selectable list of artists

--- layouts/grid/artist-profiles.php
+++ layouts/grid/artist-profiles.php
@@ -1,45 +1,61 @@
 <?php
-$artists = new WP_Query(array(
-    'post_type' => 'artists',
-    'posts_per_page' => -1,
-    'orderby' => 'title',
-    'order' => 'ASC',
-    'meta_query' => array(
-        array(
-            'key' => 'archived',
-            'value' => true,
-            'compare' => '!='
+$header = get_sub_field( 'header' );
+$headerSize = get_sub_field( 'header_size' ) ?? 'h2';
+$selectArtists = get_sub_field( 'artists' );
+
+if ( $selectArtists) :
+
+    $artists = get_sub_field( 'artists' );
+
+else :
+
+    $currentArtists = new WP_Query(array(
+        'post_type' => 'artists',
+        'posts_per_page' => -1,
+        'orderby' => 'title',
+        'order' => 'ASC',
+        'meta_query' => array(
+            array(
+                'key' => 'archived',
+                'value' => true,
+                'compare' => '!='
+            )
         )
-    )
-));
-$archivedArtists = new WP_Query(array(
-    'post_type' => 'artists',
-    'posts_per_page' => -1,
-    'orderby' => 'title',
-    'order' => 'ASC',
-    'meta_query' => array(
-        array(
-            'key' => 'archived',
-            'value' => true,
-            'compare' => '='
+    ));
+    $artists = $currentArtists->posts;
+    $archivedArtists = new WP_Query(array(
+        'post_type' => 'artists',
+        'posts_per_page' => -1,
+        'orderby' => 'title',
+        'order' => 'ASC',
+        'meta_query' => array(
+            array(
+                'key' => 'archived',
+                'value' => true,
+                'compare' => '='
+            )
         )
-    )
-));
+    ));
+
+endif;
 ?>
 
 <div class="container-lg">
-    <!-- Current Artists -->
-    <div class="row">
-        <div class="header text-center col-md-6 offset-md-3 col-lg-12 offset-lg-0">
-            <h1>Current Artists</h1>
+    <?php if( $artists ) : ?>
+        <div class="row">
+            <div class="header text-center col-md-6 offset-md-3 col-lg-12 offset-lg-0">
+                <?php if ( $selectArtists && $header ) : ?>
+                    <<?php echo $headerSize ?>><?php echo $header ?></<?php echo $headerSize ?>>
+                <?php else : ?>
+                    <h1>Current Artists</h1>
+                <?php endif; ?>
+            </div>
         </div>
-    </div>
 
-    <?php if( $artists->found_posts ) : ?>
         <div class="row">
             <div class="col-sm-10 offset-sm-1 col-lg-12 offset-lg-0">
                 <div class="row">
-                    <?php foreach ( $artists->posts as $key => $artist ) : ?>
+                    <?php foreach ( $artists as $key => $artist ) : ?>
                         <div class="grid col-6 col-sm-4 col-md-3">
                             <?php include( 'artist-profiles-card.php' ); ?>
                         </div>
@@ -49,14 +65,13 @@ $archivedArtists = new WP_Query(array(
         </div>
     <?php endif; ?>
 
-    <!-- Archived Artists -->
-    <div class="row">
-        <div class="header text-center col-md-6 offset-md-3 col-lg-12 offset-lg-0">
-            <h1>Archived Artists</h1>
+    <?php if( isset( $archivedArtists ) && $archivedArtists->found_posts ) : ?>
+        <div class="row">
+            <div class="header text-center col-md-6 offset-md-3 col-lg-12 offset-lg-0">
+                <h1>Archived Artists</h1>
+            </div>
         </div>
-    </div>
 
-    <?php if( $archivedArtists->found_posts ) : ?>
         <div class="row">
             <div class="col-sm-10 offset-sm-1 col-lg-12 offset-lg-0">
                 <div class="row">
