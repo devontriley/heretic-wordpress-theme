9e5249a4a7e59cf5b27b7dc61347f3a7c0fef7b9
devonriley <devontriley@gmail.com>
2023-04-21 10:56:08 -0400

    Fix php warning by changing image size to 'thumbnail'

--- layouts/stats/template.php
+++ layouts/stats/template.php
@@ -41,7 +41,7 @@ endswitch;
                                 <div class="stat-wrapper">
                                     <!-- Image -->
                                     <?php if ( $stat['icon'] ) : ?>
-                                        <?php echo wp_get_attachment_image( $stat['icon']['ID'], array( 50 ), false ); ?>
+                                        <?php echo wp_get_attachment_image( $stat['icon']['ID'], 'thumbnail', false ); ?>
                                     <?php endif; ?>
                                     <!-- Header -->
                                     <?php if ( $stat['header'] ) :?>
