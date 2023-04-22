56e33c11b3d032abae998d3833f145f77280a970
devonriley <devontriley@gmail.com>
2023-04-21 20:19:26 -0400

    Only show header for Code Block module when a header is passed

--- layouts/code-block/template.php
+++ layouts/code-block/template.php
@@ -9,15 +9,17 @@ $code = get_sub_field( 'code' );
 <div class="layout-code-block layout-vertical-spacing <?php if ( $is_preview ) { echo 'is-preview'; } ?>" data-layout-count="<?php echo $layoutCounter ?>">
     <div class="container-lg">
         <div class="row">
-            <div class="header text-center col-sm-10 col-md-6 offset-sm-1 offset-md-3">
-                <<?php echo $headerSize ?>>
-                    <?php echo $header ?>
-                </<?php echo $headerSize ?>>
-                <?php
-                if ( $bodyCopy ) {
-                    echo $bodyCopy;
-                } ?>
-            </div>
+            <?php if ( $header ) : ?>
+                <div class="header text-center col-sm-10 col-md-6 offset-sm-1 offset-md-3">
+                    <<?php echo $headerSize ?>>
+                        <?php echo $header ?>
+                    </<?php echo $headerSize ?>>
+                    <?php
+                    if ( $bodyCopy ) {
+                        echo $bodyCopy;
+                    } ?>
+                </div>
+            <?php endif; ?>
         </div>
         <?php echo $code ?>
     </div>
