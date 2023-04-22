8863aa7d1da2bb6438e28364158b374345685e8c
devonriley <devontriley@gmail.com>
2023-04-22 08:52:01 -0400

    Update search template to remove old code for showing multiple post types at once

--- search.php
+++ search.php
@@ -8,8 +8,33 @@
 global $wp_query;
 $isSearchTemplate = true;
 $totalPosts = $wp_query->found_posts;
-$lastPostType = null;
-$loopIteration = 0;
+$postType = $_GET['source'];
+$gridType = '';
+$gridItemClasses = '';
+switch ( $postType ) :
+    case 'post':
+    case 'page':
+    case 'tribe_events':
+        $gridType = 'articles';
+        $gridItemClasses = 'grid col-sm-6 col-md-6 col-lg-4';
+        break;
+    case 'product':
+        $gridType = 'products';
+        $gridItemClasses = 'grid col-6 col-md-4 col-lg-3';
+        break;
+    case 'services':
+        $gridType = 'services';
+        $gridItemClasses = 'grid col-sm-6 col-md-6 col-lg-4';
+        break;
+    case 'artists':
+        $gridType = 'artist-profiles';
+        $gridItemClasses = 'grid col-6 col-sm-4 col-md-3';
+        break;
+    case 'team-members':
+        $gridType = 'team-members';
+        $gridItemClasses = 'grid col-sm-6 col-lg-3';
+        break;
+endswitch;
 ?>
 
 <div class="container-xl">
@@ -31,99 +56,36 @@ $loopIteration = 0;
 
         <?php if ( have_posts() ) : ?>
 
+            <div class="search-result-type post-type-<?php echo $postType ?>">
+            <div class="layout-grid <?php echo $gridType ?>">
+            <div class="row">
+            <div class="col-sm-10 offset-sm-1 col-lg-12 offset-lg-0">
+            <div class="row">
+
             <?php while ( have_posts() ) :
 
-                the_post();
+                the_post(); ?>
 
-                $postType = get_post_type();
-                $postCount = wp_count_posts( $postType );
-                $postTypeLink = get_home_url() . '?s=&source='.$postType;
-                $nextPost = $wp_query->posts[$loopIteration + 1];
-                $gridType = '';
-                $gridItemClasses = '';
-                $sectionHeader = ucwords( str_replace( '-', ' ', $postType ) );
-                if( substr( $sectionHeader, -1 ) !== 's' ) {
-                    $sectionHeader .= 's';
-                }
+                    <div class="<?php echo $gridItemClasses ?>">
+                        <?php
+                        /*
+                         * Include the Post-Format-specific template for the content.
+                         * If you want to override this in a child theme, then include a file
+                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                         */
+                        get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
+                        ?>
+                    </div>
 
-                switch ( $postType ) :
-                    case 'post':
-                    case 'page':
-                    case 'tribe_events':
-                        $gridType = 'articles';
-                        $gridItemClasses = 'grid col-sm-6 col-md-6 col-lg-4';
-                        break;
-                    case 'product':
-                        $gridType = 'products';
-                        $gridItemClasses = 'grid col-6 col-md-4 col-lg-3';
-                        break;
-                    case 'services':
-                        $gridType = 'services';
-                        $gridItemClasses = 'grid col-sm-6 col-md-6 col-lg-4';
-                        break;
-                    case 'artists':
-                        $gridType = 'artist-profiles';
-                        $gridItemClasses = 'grid col-6 col-sm-4 col-md-3';
-                        break;
-                    case 'services':
-                        $gridType = 'services';
-                        $gridItemClasses = 'grid col-sm-6 col-md-6 col-lg-4';
-                        break;
-                    case 'team-members':
-                        $gridType = 'team-members';
-                        $gridItemClasses = 'grid col-sm-6 col-lg-3';
-                        break;
-                endswitch;
+            <?php endwhile; ?>
 
-                if ( $postType !== $lastPostType ) : ?>
+            </div><!-- .row -->
+            </div><!-- .col -->
+            </div><!-- .row -->
+            </div><!-- .layout-grid -->
+            </div><!-- Close <?php echo $postType ?> -->
 
-                    <!-- Start <?php echo $postType ?> -->
-                    <div class="search-result-type post-type-<?php echo $postType ?>">
-                    <div class="layout-grid <?php echo $gridType ?>">
-
-<!--                    <div class="row">-->
-<!--                        <div class="col-sm-10 offset-sm-1 col-md-6 col-lg-12 offset-lg-0">-->
-<!--                            <div class="search-results-type-header">-->
-<!--                                <h2>--><?php //echo $sectionHeader ?><!--</h2>-->
-<!--                                <a href="--><?php //echo $postTypeLink ?><!--">View all results</a>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-
-                    <div class="row">
-                    <div class="col-sm-10 offset-sm-1 col-lg-12 offset-lg-0">
-                    <div class="row">
-
-                <?php endif; ?>
-
-                        <div class="<?php echo $gridItemClasses ?>">
-                            <?php
-                            /*
-                             * Include the Post-Format-specific template for the content.
-                             * If you want to override this in a child theme, then include a file
-                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                             */
-                            get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
-                            ?>
-                        </div>
-
-                <?php
-                /* When the next post's type doesn't match the current post's type,
-                we close the container divs so we can begin a new section on the next iteration */
-                if ( $nextPost->post_type !== $postType ) : ?>
-                    </div><!-- .row -->
-                    </div><!-- .col -->
-                    </div><!-- .row -->
-                    </div><!-- .layout-grid -->
-                    </div><!-- Close <?php echo $postType ?> -->
-                <?php endif; ?>
-
-                <?php
-                $lastPostType = $postType;
-                $loopIteration++;
-
-            endwhile;
-
+            <?php
             // Previous/next page navigation.
             heretic_the_posts_navigation();
 
