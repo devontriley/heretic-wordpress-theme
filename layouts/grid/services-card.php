3c6cf8974161cfea89c400748570c0d04099ec71
devonriley <devontriley@gmail.com>
2023-04-30 14:28:12 -0400

    Replace service pills with excerpt

--- layouts/grid/services-card.php
+++ layouts/grid/services-card.php
@@ -1,4 +1,5 @@
 <?php
+$serviceExcerpt = get_field( 'service_excerpt', $p->ID );
 $fImage = wp_get_attachment_image( get_post_thumbnail_id( $p->ID ), 'full' );
 $featuredImage = get_the_post_thumbnail( $p->ID );
 $serviceCats = wp_get_post_terms( $p->ID, array(
@@ -21,17 +22,19 @@ $serviceCats = wp_get_post_terms( $p->ID, array(
         <?php if( $p->post_title ) : ?>
             <h3 class="card-title"><?php echo $p->post_title ?></h3>
         <?php endif; ?>
-        <!-- Categories -->
-        <?php if ( $serviceCats ) : ?>
-            <ul class="service-category-pills">
-                <?php foreach ( $serviceCats as $serviceCat ) : ?>
-                    <?php if ( $serviceCat->parent !== 0 ) : ?>
-                        <li><?php echo $serviceCat->name ?></li>
-                    <?php endif; ?>
-                <?php endforeach; ?>
-            </ul>
+        <?php if ( $serviceExcerpt ) : ?>
+            <p class="excerpt"><?php echo $serviceExcerpt ?></p>
         <?php endif; ?>
+<!--        --><?php //if ( $serviceCats ) : ?>
+<!--            <ul class="service-category-pills">-->
+<!--                --><?php //foreach ( $serviceCats as $serviceCat ) : ?>
+<!--                    --><?php //if ( $serviceCat->parent !== 0 ) : ?>
+<!--                        <li>--><?php //echo $serviceCat->name ?><!--</li>-->
+<!--                    --><?php //endif; ?>
+<!--                --><?php //endforeach; ?>
+<!--            </ul>-->
+<!--        --><?php //endif; ?>
     </div>
 </div>
 
-<?php unset( $featuredImage ); ?>
+<?php unset( $fImage, $featuredImage, $serviceExcerpt, $serviceCats, $p ); ?>
