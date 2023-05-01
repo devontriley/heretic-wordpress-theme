f0f744f153ec97c975647b44ebee11741c8be0e1
devonriley <devontriley@gmail.com>
2023-05-01 07:13:28 -0400

    Update primary nav styling

--- template-parts/header/site-nav.php
+++ template-parts/header/site-nav.php
@@ -45,15 +45,21 @@ if( $nav ) : ?>
             <li class="<?php if ( $value['children'] ) { echo 'has-children'; } ?>">
                 <?php if ( $value['url'] !== '#' ) { ?>
                 <a href="<?php echo $value['url'] ?>" target="<?php echo $value['target'] ?>">
+                <?php } else { ?>
+                <span>
                 <?php } ?>
+
                     <?php echo $value['title'] ?>
                     <?php if ( $value['children'] ) : ?>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
                         </svg>
                     <?php endif; ?>
+
                 <?php if ( $value['url'] !== '#' ) { ?>
                 </a>
+                <?php } else { ?>
+                </span>
                 <?php } ?>
 
                 <?php if ( $value['children'] ) : ?>
