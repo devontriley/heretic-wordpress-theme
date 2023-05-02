99b3f261646d4c92dced57b88402149466e254d9
devonriley <devontriley@gmail.com>
2023-05-02 14:11:09 -0400

    Add header class to logo grid header

--- layouts/grid/logos.php
+++ layouts/grid/logos.php
@@ -1,7 +1,7 @@
 <div class="container-lg">
     <?php if ( $header ) : ?>
     <div class="row">
-        <div class="col-sm-10 offset-sm-1 col-lg-12 offset-lg-0 text-center">
+        <div class="col-sm-10 offset-sm-1 col-lg-12 offset-lg-0 header text-center">
             <<?php echo $headerSize ?>>
                 <?php echo $header ?>
             </<?php echo $headerSize ?>>
