baa8ac9b54a26db835c24616631c105c0a947c61
devonriley <devontriley@gmail.com>
2023-05-04 15:58:24 -0400

    Add favicons

--- header.php
+++ header.php
@@ -16,6 +16,16 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <?php
+    $faviconPrefix = get_field( 'favicon_prefix', 'options' );
+    $faviconPath = get_stylesheet_directory_uri() . '/favicon/' . $faviconPrefix . '_';
+    ?>
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $faviconPath ?>apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $faviconPath ?>favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $faviconPath ?>favicon-16x16.png">
+    <link rel="manifest" href="<?php echo $faviconPath ?>site.webmanifest">
+
 	<?php wp_head(); ?>
 </head>
 
