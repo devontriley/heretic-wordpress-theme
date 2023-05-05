00ff59beb23cde82502b01fb91e2896ab1e3ecfa
devonriley <devontriley@gmail.com>
2023-05-05 08:10:21 -0400

    Add body copy field to Video module

--- layouts/video/template.php
+++ layouts/video/template.php
@@ -2,13 +2,19 @@
 global $layoutCounter;
 
 $video = get_sub_field('video');
+$bodyCopy = get_sub_field( 'body_copy' );
 ?>
 
 <div class="layout-video layout-vertical-spacing <?php if ( $is_preview ){ echo 'is-preview'; } ?>" data-layout-count="<?php echo $layoutCounter ?>">
     <div class="container-lg">
         <div class="row">
-            <div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2">
+            <div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 text-center">
                 <?php echo $video ?>
+                <?php if ( $bodyCopy ) : ?>
+                    <div class="body-copy">
+                        <?php echo $bodyCopy ?>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
