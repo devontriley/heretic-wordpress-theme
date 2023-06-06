b57889ffe6bf745942300c3e9dabe2927dc10c97
devonriley <devontriley@gmail.com>
2023-06-06 11:56:34 -0400

    Remove hover state from list item with sub sub menu

--- template-parts/header/site-nav.php
+++ template-parts/header/site-nav.php
@@ -85,7 +85,11 @@ endif; ?>
                             <?php foreach ( $value['children'] as $child ) :
                                 $icon = get_field('menu_item_icon', $child['id']); ?>
                                 <li>
-                                    <a href="<?php echo $child['url'] ?>" target="<?php echo $child['target'] ?>">
+                                    <?php if ( $child['url'] !== '#' ) : ?>
+                                    <a href="<?php echo $child['url'] ?>" class="<?php if ($child['children']){ echo 'has-children'; }?>" target="<?php echo $child['target'] ?>">
+                                    <?php else : ?>
+                                    <div>
+                                    <?php endif; ?>
                                         <?php if ( $icon ) : ?>
                                             <?php echo wp_get_attachment_image( $icon['ID'], array(25,25), '', array( 'class' => 'sub-menu-icon' ) ) ?>
                                         <?php else : ?>
@@ -97,7 +101,11 @@ endif; ?>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                             <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                                         </svg>
+                                    <?php if ( $child['url'] !== '#' ) : ?>
                                     </a>
+                                    <?php else : ?>
+                                    </div>
+                                    <?php endif;?>
 
                                     <?php if ( $child['children'] ) : ?>
                                         <ul class="sub-sub-menu">
