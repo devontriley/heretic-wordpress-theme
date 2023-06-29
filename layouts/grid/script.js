c86b6b890e9f9b04015d3fe0363aa94f76e2269f
devonriley <devontriley@gmail.com>
2023-06-29 18:23:39 -0400

    Cleaned up grid js

--- layouts/grid/script.js
+++ layouts/grid/script.js
@@ -1,45 +1,39 @@
 const teamMemberDialog = document.getElementById("team-members-dialog");
+if (teamMemberDialog) {
+    const openButtons = document.querySelectorAll(".team-member-open-drawer");
+    const closeButton = document.querySelector(".close-team-members-dialog");
 
-if ( teamMemberDialog ) {
-    const teamMemberOpenButtons = document.querySelectorAll(".team-member-open-drawer");
-    const closeTeamMembersDialogButton = document.querySelector('.close-team-members-dialog')
+    closeButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        teamMemberDialog.close();
+    });
 
-    closeTeamMembersDialogButton.addEventListener('click', e => {
-        e.preventDefault()
-
-        teamMemberDialog.close()
-    })
-
-    teamMemberOpenButtons.forEach(button => {
-        button.addEventListener('click', e => {
-            console.log(e)
-            e.preventDefault()
-
-            let id = e.target.dataset.id;
+    openButtons.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+            const id = e.target.dataset.id;
             if (id) {
-                let member = teamMemberDialog.querySelector('.team-member[data-id="' + id + '"]');
-                member.classList.add("d-block");
+                teamMemberDialog.querySelector('.team-member[data-id="' + id + '"]').classList.add("d-block");
             }
+            teamMemberDialog.showModal();
+        });
+    });
 
-            teamMemberDialog.showModal()
-        })
-    })
+    teamMemberDialog.addEventListener("close", (e) => {
+        teamMemberDialog.querySelectorAll(".team-member").forEach((member) => {
+            member.classList.remove("d-block");
+        });
+    });
 
-    teamMemberDialog.addEventListener('close', e => {
-        teamMemberDialog.querySelectorAll('.team-member').forEach(member => {
-            member.classList.remove('d-block')
-        })
-    })
-
-    teamMemberDialog.addEventListener('click', e => {
-        const dialogDimensions = teamMemberDialog.getBoundingClientRect()
-        if(
-            e.clientX < dialogDimensions.left ||
-            e.clientX > dialogDimensions.right ||
-            e.clientY < dialogDimensions.top ||
-            e.clientY > dialogDimensions.bottom
+    teamMemberDialog.addEventListener("click", (e) => {
+        const rect = teamMemberDialog.getBoundingClientRect();
+        if (
+            e.clientX < rect.left ||
+            e.clientX > rect.right ||
+            e.clientY < rect.top ||
+            e.clientY > rect.bottom
         ) {
-            teamMemberDialog.close()
+            teamMemberDialog.close();
         }
-    })
+    });
 }
